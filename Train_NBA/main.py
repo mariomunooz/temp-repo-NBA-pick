748dbf1d317ac59317a193340a033d7dddb21fa5
mariomunooz <72894032+mariomunooz@users.noreply.github.com>
2024-06-05 17:55:15 +0200

    Add files via upload

--- Train_NBA/main.py
+++ Train_NBA/main.py
@@ -1,19 +1,24 @@
 from Game import Game
 import pandas as pd
-# import argparse
-#
-# parser = argparse.ArgumentParser(description='Process arguments about an NBA game.')
-# parser.add_argument('--path', type=str,
-#                     help='a path to json file to read the events from',
-#                     required = True)
-# parser.add_argument('--event', type=int, default=0,
-#                     help="""an index of the event to create the animation to
-#                             (the indexing start with zero, if you index goes beyond out
-#                             the total number of events (plays), it will show you the last
-#                             one of the game)""")
-#
-# args = parser.parse_args()
+import os
+
 train = pd.read_csv('train.csv')
+
+data_path = "C:\\Users\\u172951\\Documents\\data"
+
+
+
+train['Game_ID'] = train['Game_ID'].apply(lambda x: os.path.join(data_path, x))
+
+files = [os.path.join(data_path, file) for file in os.listdir(data_path)]
+
+print(files)
+
+train = train[train['Game_ID'].isin(files)]
+
+train = train.reset_index(drop=True)
+
+
 for index,row in train.iterrows():
     print("Game ID: {}, Event Index: {}, Frame Number: {} {}/{}".format(row.Game_ID,row.Event_Number,row.Frame_Number,index,len(train)))
     game = Game(path_to_json=row.Game_ID, event_index=row.Event_Number, frame=row.Frame_Number)
@@ -24,5 +29,8 @@ for index,row in train.iterrows():
     for i in range(len(features)):
         str_col = 'f' + str(i)
         train.at[index,str_col] = features[i]
+    
 train = train.dropna(axis=0)
 train.to_csv('train_fin.csv',index=False)
+
+print('finished')
